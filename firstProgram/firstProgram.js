19b60132dd929ded86b9ee59b6f4fe231b2da1b8
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-02 23:10:24 +0600

    First Program in ts

---
+++ firstProgram/firstProgram.js
@@ -0,0 +1,9 @@
+var hello;
+var world;
+var id;
+// hello = 1213; // always give error (worng way)
+hello = "Hello"; //Right ways
+world = "Miz Typescript";
+// id="1618"; / always give error (worng way)
+id = 1718; //Right ways
+console.log(hello + world + "Your Id is " + id);
---
+++ firstProgram/firstProgram.ts
@@ -0,0 +1,9 @@
+let hello: string;
+let world: string;
+let id: number;
+// hello = 1213; // always give error (worng way)
+hello = "Hello"; //Right ways
+world = "Miz Typescript";
+// id="1618"; / always give error (worng way)
+id = 1718; //Right ways
+console.log(hello + world + "Your Id is " + id);
