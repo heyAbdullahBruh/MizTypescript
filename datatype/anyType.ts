79d959c563d675184f88b29e69fd51cb25efdaf8
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-04 23:40:23 +0600

    ENUM Data type

---
+++ datatype/anyType.ts
@@ -0,0 +1,13 @@
+//  Any Data type ---> 
+
+let username : any ; 
+username = 'Hello';
+username =1121;
+username=true;
+username=null;
+username=undefined;
+username=NaN;
+username=null;
+
+
+console.log(username);
