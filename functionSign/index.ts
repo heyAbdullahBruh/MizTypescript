eea75f82da87c8283ae452c5e72cfe655ba031bc
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-09 21:25:10 +0600

    Typescript function signeture start

---
+++ functionSign/index.ts
@@ -0,0 +1,8 @@
+//  TypeScript function signeture --->
+//  function signeture define which parameter or value I receved or send in function .
+
+let User: () => void;
+
+User = () => {
+  console.log("Hello Mr");
+};
