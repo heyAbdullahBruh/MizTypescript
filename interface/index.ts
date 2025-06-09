cc82353579a9e6b3c911149043151fa03aa25f2a
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-09 22:53:56 +0600

    Typescript interface 1.2(with interface)

--- interface/index.ts
+++ interface/index.ts
@@ -3,7 +3,7 @@
 
 // without interface --->
 
-let users: { name: string; age: string | number; bloodGroup: string }[] = [];
+/*let users: { name: string; age: string | number; bloodGroup: string }[] = [];
 
 let user1: { name: string; age: string | number; bloodGroup: string } = {
   name: "Mohammad TOha",
@@ -30,7 +30,44 @@ function userShow(user: {
   console.log(
     `User -> Name : ${user?.name}, Age : ${user?.age}, BloodGroup : ${user?.bloodGroup}`
   );
-} /**User -> Name : Mohammad TOha, Age : 37, BloodGroup : A+
-User -> Name : Asef Saikat, Age : 40, BloodGroup : A- */
+ }// User -> Name : Mohammad TOha, Age : 37, BloodGroup : A+
+// User -> Name : Asef Saikat, Age : 40, BloodGroup : A- 
+
+users.forEach((u) => userShow(u));
+*/
+
+//  With InterFace -->
+
+interface IUser {
+  name: string;
+  age: string | number;
+  bloodGroup: string;
+}
+
+let users: IUser[] = [];
+
+let user1: IUser = {
+  name: "Mohammad TOha",
+  age: 37,
+  bloodGroup: "A+",
+};
+
+let user2: IUser = {
+  name: "Asef Saikat",
+  age: 40,
+  bloodGroup: "A-",
+};
+
+users.push(user1);
+users.push(user2);
+
+// console.log(users); // [{ name: 'Mohammad TOha', age: 37, bloodGroup: 'A+' },{ name: 'Asef Saikat', age: 40, bloodGroup: 'A-' }]
+
+function userShow(user: IUser): void {
+  console.log(
+    `User -> Name : ${user?.name}, Age : ${user?.age}, BloodGroup : ${user?.bloodGroup}`
+  );
+} // User -> Name : Mohammad TOha, Age : 37, BloodGroup : A+
+// User -> Name : Asef Saikat, Age : 40, BloodGroup : A-
 
 users.forEach((u) => userShow(u));
