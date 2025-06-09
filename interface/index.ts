22f446739d863e5f3c226883872bbfb163e7e58e
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-09 22:55:02 +0600

    Typescript interface 1.2(with interface)

--- interface/index.ts
+++ interface/index.ts
@@ -1,40 +1,39 @@
 // Typscript interface ..
 //  Interface to make code effecient
 
-// without interface --->
+/*/////// without interface --->*/
 
-/*let users: { name: string; age: string | number; bloodGroup: string }[] = [];
+// let users: { name: string; age: string | number; bloodGroup: string }[] = [];
 
-let user1: { name: string; age: string | number; bloodGroup: string } = {
-  name: "Mohammad TOha",
-  age: 37,
-  bloodGroup: "A+",
-};
+// let user1: { name: string; age: string | number; bloodGroup: string } = {
+//   name: "Mohammad TOha",
+//   age: 37,
+//   bloodGroup: "A+",
+// };
 
-let user2: { name: string; age: string | number; bloodGroup: string } = {
-  name: "Asef Saikat",
-  age: 40,
-  bloodGroup: "A-",
-};
+// let user2: { name: string; age: string | number; bloodGroup: string } = {
+//   name: "Asef Saikat",
+//   age: 40,
+//   bloodGroup: "A-",
+// };
 
-users.push(user1);
-users.push(user2);
+// users.push(user1);
+// users.push(user2);
 
-// console.log(users); // [{ name: 'Mohammad TOha', age: 37, bloodGroup: 'A+' },{ name: 'Asef Saikat', age: 40, bloodGroup: 'A-' }]
+// // console.log(users); // [{ name: 'Mohammad TOha', age: 37, bloodGroup: 'A+' },{ name: 'Asef Saikat', age: 40, bloodGroup: 'A-' }]
 
-function userShow(user: {
-  name: string;
-  age: string | number;
-  bloodGroup: string;
-}): void {
-  console.log(
-    `User -> Name : ${user?.name}, Age : ${user?.age}, BloodGroup : ${user?.bloodGroup}`
-  );
- }// User -> Name : Mohammad TOha, Age : 37, BloodGroup : A+
-// User -> Name : Asef Saikat, Age : 40, BloodGroup : A- 
+// function userShow(user: {
+//   name: string;
+//   age: string | number;
+//   bloodGroup: string;
+// }): void {
+//   console.log(
+//     `User -> Name : ${user?.name}, Age : ${user?.age}, BloodGroup : ${user?.bloodGroup}`
+//   );
+//  }// User -> Name : Mohammad TOha, Age : 37, BloodGroup : A+
+// // User -> Name : Asef Saikat, Age : 40, BloodGroup : A-
 
-users.forEach((u) => userShow(u));
-*/
+// users.forEach((u) => userShow(u));
 
 //  With InterFace -->
 
